99f528869e37856dd8424a49f8092744467c10e3
Filippo <filippo@pixel-plus.ch>
2025-06-27 17:10:30 +0200

    chore: more cleanup

--- lib/themes/assets.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ImageAssets {
-  static String imageEndPoint = 'assets/common/';
-
-  static Image icLogoZuerich({Color? color}) =>
-      Image.asset('${imageEndPoint}logo_zuerich.png', color: color);
-}
